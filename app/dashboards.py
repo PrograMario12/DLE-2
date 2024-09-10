cabdfb219deec3410e364037dc037f1256ed2528
Mario Abarca <mario.abarca@magna.com>
2024-09-10 11:26:33 -0600

    Add PEP8 to dashboards

--- app/dashboards.py
+++ app/dashboards.py
@@ -1,3 +1,5 @@
+''' This module contains the routes for the dashboards of the app. '''
+
 from flask import Blueprint, render_template, request
 from app import functions
 
@@ -5,19 +7,17 @@ dashboards_bp = Blueprint('settings', __name__)
 
 @dashboards_bp.route('/visualizaciones')
 def dashboard_lines():
+    ''' Renders the dashboard page for the lines. '''
     results = functions.get_lines()
     number_of_lines = len(results)
     employees_for_line = functions.get_employees_for_line()
-    employees_for_line = {line[0]: line[1] for line in 
+    employees_for_line = {line[0]: line[1] for line in
                           employees_for_line}
 
     there_are_no_lines = []
 
     lines = []
-    '''
-        result[0] is equal to the line id
-        result[2] is equal to the line capacity
-    '''
+
     for result in results:
         line = [result[1], result[2]]
         if result[1] in employees_for_line:
@@ -25,24 +25,21 @@ def dashboard_lines():
         else:
             line.append(0)
 
-        #Here we calculate the number of available operators
-        # line[1] = int(line[1]) - int(line[2])
-
-        #Here we check if the line has available operators
         lines.append(line)
 
     context = {
         'css_file': 'static/css/styles.css',
-        'tipo_seleccion': 'línea',
+        'selection_type': 'línea',
         'num_cards': number_of_lines,
-        'lineas_capacidad_operadores': lines,
+        'inline_operator_capacity': lines,
         'lineas': there_are_no_lines
     }
 
     return render_template('visualizaciones.html', **context)
 
-@dashboards_bp.route('/visualizacionesEstacion')
+@dashboards_bp.route('/visualizaciones_estación')
 def dashboard_stations():
+    ''' Renders the dashboard page for the stations. '''
     line = request.args.get('line')
     line_name = functions.get_line_id(line)
 
@@ -50,54 +47,77 @@ def dashboard_stations():
     numbers_of_stations = len(results)
 
     employees_for_station = functions.get_employees_for_station(line)
-    employees_for_station = {station[0]: station[1] for station in 
-                             employees_for_station}
+    employees_for_station = {station[0]: station[1] for station
+                             in employees_for_station}
     stations_list = sorted(list(set([result[0] for result in results])))
 
-    stations = []
-    for result in results:
-        station = result[0]
-        capacity_LH = result[1]
-        operators_LH = employees_for_station.get(str(station) 
-                                                 + " LH", 0)
-        capacity_RH = result[2]
-        operators_RH = employees_for_station.get(str(station) 
-                                                 + " RH", 0)
+    stations = prepare_stations_data(results, employees_for_station,
+                                     line)
 
-        names_operators_LH = functions.get_names_operators(station,
-                                                           line, 'LH')
-
-        names_operators_RH = functions.get_names_operators(station, 
-                                                           line, 'RH')
-
-        capacity_LH = int(capacity_LH) - int(operators_LH)
-        capacity_RH = int(capacity_RH) - int(operators_RH)
-
-        stations.append([station, capacity_LH, operators_LH, 
-                         capacity_RH, operators_RH, names_operators_LH, 
-                         names_operators_RH])
-
-    employees_for_line = functions.get_employees_for_line(line)
-    employees_for_line = (
-        int(employees_for_line[0][1])
-        if employees_for_line
-        else 0
-    )
-
-    employees_necessary = int(functions.get_employees_necessary_for_line
-                              (line)[0][0])
-
-    print('Las stations son:', stations)
+    employees_for_line = get_employees_for_line(line)
+    employees_necessary = int(
+            functions.get_employees_necessary_for_line(line)[0][0]
+        )
 
     context = {
         'css_file': 'static/css/styles.css',
-        'tipo_seleccion': 'estación',
+        'selection_type': 'estación',
         'num_cards': numbers_of_stations,
-        'lineas_capacidad_operadores': stations,
+        'inline_operator_capacity': stations,
         'lineas': stations_list,
-        'lineaseleccionada': line,
+        'selected_line': line,
         'employees_for_line': employees_for_line,
         'employees_necessary': employees_necessary
     }
 
     return render_template('visualizaciones.html', **context)
+
+
+def prepare_stations_data(results, employees_for_station, line):
+    ''' Prepares the data for the stations. '''
+    stations = []
+    for result in results:
+        station = result[0]
+        capacity_lh, operators_lh = get_capacity_operators(
+                                                result[1],
+                                                station,
+                                                employees_for_station,
+                                                ' LH'
+                                            )
+        capacity_rh, operators_rh = get_capacity_operators(
+                                                result[2],
+                                                station,
+                                                employees_for_station,
+                                                ' RH'
+                                            )
+
+        names_operators_lh = functions.get_names_operators(
+                                                station,
+                                                line,
+                                                'LH'
+                                            )
+        names_operators_rh = functions.get_names_operators(
+                                                station,
+                                                line,
+                                                'RH'
+                                            )
+
+        stations.append([station, capacity_lh, operators_lh,
+                         capacity_rh, operators_rh, names_operators_lh,
+                         names_operators_rh])
+
+    return stations
+
+
+def get_capacity_operators(capacity, station, employees_for_station,
+                           suffix):
+    ''' Gets the capacity and operators for a station. '''
+    operators = employees_for_station.get(f"{station}{suffix}", 0)
+    capacity = int(capacity) - int(operators)
+    return capacity, operators
+
+
+def get_employees_for_line(line):
+    ''' Gets the employees for a line. '''
+    employees_for_line = functions.get_employees_for_line(line)
+    return int(employees_for_line[0][1]) if employees_for_line else 0
