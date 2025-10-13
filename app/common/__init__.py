eb72b4fc48563e422ace9f0a69033cc53fcf4187
mariabar <mario.abarca@magna.com>
2025-10-13 11:10:37 -0600

    Docs about commun folder

---
+++ app/common/__init__.py
@@ -0,0 +1,7 @@
+"""
+La carpeta common generalmente se utiliza para almacenar código,
+utilidades o excepciones que son compartidas entre diferentes módulos o
+componentes de la aplicación. Aquí se colocan funciones, clases o
+constantes reutilizables para evitar duplicidad y centralizar lógica
+común.
+"""
